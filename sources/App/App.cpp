7e20902cc44ded3c3ad03be7dd2f4f1c2ca707be
FiendChain <formulating.response@gmail.com>
2018-09-27 00:01:45 +1000

    Implemented index buffer

--- sources/App/App.cpp
+++ sources/App/App.cpp
@@ -23,36 +23,28 @@ void App::render() {
         {-0.5f, -0.5f},
         { 0.5f, -0.5f},
         { 0.5f,  0.5f},
-        // render top half of square
-        { 0.5f,  0.5f},
-        {-0.5f,  0.5f},
-        {-0.5f, -0.5f},
+        {-0.5f,  0.5f}, // use an index buffer to reuse prev vertexes
+    };
+    // induces
+    unsigned int indices[] = {
+        0, 1, 2, // bottom triangle
+        2, 3, 0, // top triangle
     };
     // creating and assigning data
-    unsigned int buffer_id;
-    glGenBuffers(1, &buffer_id);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
-    // void glBufferData(GLenum target,     // tyoe of data
- 	//                   GLsizeiptr size,   // size of buffer
- 	//                   const GLvoid * data, // pointer to data
- 	//                   GLenum usage);     // usage pattern
-    // http://docs.gl/gl4/glBufferData
-    // Usage specified the expected usage pattern of the data
-    // GL_STATIC_DRAW = modified once and drawn many times
-    // etc...
+    unsigned int bufferId;
+    glGenBuffers(1, &bufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, bufferId);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data, GL_STATIC_DRAW);
-    
-    // define vertex layout
-    // void glVertexAttribPointer(GLuint index,
- 	//                            GLint size,   // total indexes, not sizeof
- 	//                            GLenum type,  // GL_<TYPE>
- 	//                            GLboolean normalized, // within -1 and 1
- 	//                            GLsizei stride, // size of the vertex
- 	//                            const GLvoid * pointer); // value of offset to the attribute, not a pointer actually
+    // vertex layout
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(struct Position), (const void*)offsetof(VertexData, position));
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
-
+    // create index buffer object
+    unsigned int ibo; // index buffer object
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // load shaders
     struct ShaderFile shaderFile = parseShaderFile("resources/shaders/basicShader.glsl");
     unsigned int programId = createProgram(shaderFile.vertex, shaderFile.fragment);
     glUseProgram(programId);
@@ -62,8 +54,8 @@ void App::render() {
     {
         // clear window
         glClear(GL_COLOR_BUFFER_BIT);
-        // render stuff
-        glDrawArrays(GL_TRIANGLES, 0, sizeof(vertex_data)/sizeof(VertexData));
+        // render using index buffer
+        glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(unsigned int), GL_UNSIGNED_INT, nullptr);
         // swap double buffer (front and back)
         glfwSwapBuffers(window); 
         // poll events
