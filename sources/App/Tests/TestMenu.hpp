5fe34deac801385575d77a36b6dc55306d23d762
FiendChain <formulating.response@gmail.com>
2018-10-02 20:24:56 +1000

    Added static assert to TestMenu to only allow Tests to be added

--- sources/App/Tests/TestMenu.hpp
+++ sources/App/Tests/TestMenu.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "Test.hpp"
 #include <unordered_map>
+#include <type_traits>
 #include <memory>
 
 namespace test
@@ -23,6 +24,7 @@ class TestMenu: public Test
         template <typename T, typename... Args>
         void AddTest(const std::string& name, Args&& ... args)
         {
+            static_assert(std::is_base_of<Test, T>::value, "Not derived from Test");
             m_TestList.insert(std::make_pair(name, (TestEntry){std::make_unique<T>(std::forward<Args>(args)...), false}));
         }
         void OnUpdate(float deltaTime) override;
